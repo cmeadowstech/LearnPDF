9126706330bc3eddc6b77494e5c02f2c4e1b9eae
Cody Meadows <codyrm@gmail.com>
2023-11-04 19:01:39 +0000

    Alpha release

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Script to convert MSFT Learn modules to PDFs. Might need more tweaking and testi
 1. Install pip dependencies from requirements.txt
 2. Calibre - https://calibre-ebook.com/download
     - Specifically this script uses the ebook-convert CLI tool
+- Optional: Install Amazon's Bookerly font - https://developer.amazon.com/en-US/alexa/branding/echo-guidelines/identity-guidelines/typography
+  - Shouldn't break anything if you don't have it, but let me know and I might be able to add some logic to check
 
 ## How to use
 Once the requiremments are installed, you can simply run with the url as a commandline argument.
@@ -13,3 +15,10 @@ Once the requiremments are installed, you can simply run with the url as a comma
 ```
 python3 main.py 'https://learn.microsoft.com/en-us/training/paths/az-400-get-started-devops-transformation-journey/'
 ```
+
+Alternatively, you can pass a 'Series' name after the URL if you want to keep track of which Certfication the PDFs are for with your EBook manager.
+```
+python3 main.py 'https://learn.microsoft.com/en-us/training/paths/az-400-get-started-devops-transformation-journey/' 'AZ-400'
+```
+
+The structure of these paths is Learning Path -> Module -> Units. PDFs should have a Table of Content with the chapters being the Module names, and sub-chapters being the unit names
--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import urllib.parse
 import requests
 import sys
 
-url = str(sys.argv[1])
+learnPath = str(sys.argv[1])
+
+if len(sys.argv) == 3:
+    series = str(sys.argv[2])
+else:
+    series = "Microsoft Learn"
 
 # Create folders this script uses to export files
 
@@ -53,12 +58,32 @@ def cleanModule(unitUrl):
     for u in uselessDivs:
         soup.find(id=u).decompose()
 
+    # Remove scripts and links
+    for tag in soup(["link", "script"]):
+        tag.decompose()
+
     # Add url to unit title
     header = soup.h1
     newTag = soup.new_tag("a", href=unitUrl)
     newTag.string = soup.h1.string
     header.clear()
     header.append(newTag)
+    header.name = "h3"
+
+    # Change h2 to h4
+    h3 = soup.find_all("h2")
+    for h in h3:
+        h.name = "h4"
+
+    # Fix links
+    links = soup.find_all("a")
+
+    for link in links:
+        try:
+            if link["data-linktype"] in ["absolute-path", "relative-path"]:
+                link["href"] = urllib.parse.urljoin(unitUrl, link["href"])
+        except:
+            pass
 
     return soup
 
@@ -92,39 +117,62 @@ def exportHtml(html, file):
 
 def main(url):
     soup = createSoup(url)
+    fileName = (
+        soup.find("h1").get_text().strip().replace(" ", "-")
+    )  # Get filename before conversion otherwise nothing is found
+
+    # Find and extract title. Yeah this is really hacky.
+    title = soup.h1
+    title.string.wrap(soup.new_tag("a"))
+    title.a["href"] = url
+    title.a.wrap(soup.new_tag("h1"))
+    title.h1.wrap(soup.new_tag("html"))
+    title = title.html.extract()
+
+    # Process and export HTML. Deletes file first if already exists
+
+    try:
+        os.remove(f"HTML/{fileName}.html")
+    except FileNotFoundError:
+        pass
+
+    exportHtml(title, fileName)
 
     modules = soup.find_all(attrs={"data-bi-name": "module"})
 
     for module in modules:
+        # Find unit urls for this module
         url = module.find_all("a", limit=1)
         url = "https://learn.microsoft.com/en-us/training/" + url[0]["href"].lstrip(
             "./"
         )
 
         soup = createSoup(url)
+
+        # Add h2 for module title
+        moduleTitle = soup.h1
+        moduleTitle.string.wrap(soup.new_tag("a"))
+        moduleTitle.a["href"] = url
+        moduleTitle.a.wrap(soup.new_tag("h2"))
+        moduleTitle.h2.wrap(soup.new_tag("html"))
+        moduleTitle = moduleTitle.html.extract()
+        exportHtml(moduleTitle, fileName)
+
         units = soup.find(id="unit-list")
         units = units.find_all("a")
-
-        # Find and extract title
-        title = soup.find("h1").get_text().strip()
-
-        # Process and export HTML. Deletes file first if already exists
-        fileName = title.replace(" ", "-")
-
-        try:
-            os.remove(f"HTML/{fileName}.html")
-        except FileNotFoundError:
-            pass
 
         for unit in units:
             cleaned = cleanModule(url + unit["href"])
             downloadImg(cleaned, url + unit["href"])
             exportHtml(cleaned, fileName)
 
-        # Converts the HTML to PDF
-        os.system(
-            f"ebook-convert HTML/{fileName}.html PDF/{fileName}.pdf --page-breaks-before '//h:h1[position()>1]' --chapter '//h:h1' --chapter-mark 'rule' --use-auto-toc --pretty-print"
-        )
+    # Converts the HTML to PDF
+    os.system(
+        f"ebook-convert 'HTML/{fileName}.html' 'PDF/{fileName}.pdf' --page-breaks-before '//h:h2[position()>1]' \
+        --chapter '//h:h2' --use-auto-toc --level1-toc '//h:h2' --level2-toc '//h:h3' \
+        --pdf-page-numbers --pretty-print --pdf-serif-family 'Bookerly' --base-font-size 10\
+        --authors 'Microsoft' --title '{title.find('a').string}' --series '{series}'"
+    )
 
 
-main(url)
+main(learnPath)
